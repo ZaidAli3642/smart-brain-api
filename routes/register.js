7ea50c6cb076291c277328a5c4818c6d6b3a1b15
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 02:08:21 +0500

    fixing

--- routes/register.js
+++ routes/register.js
@@ -42,7 +42,7 @@ route.post("/register", (req, res) => {
         })
         .then(trx.commit)
         .catch(trx.rollback);
-    }).catch((err) => res.status(400).json("Unable to Register!"));
+    }).catch((err) => res.status(400).json("Unable to Register!", err));
   });
 });
 
