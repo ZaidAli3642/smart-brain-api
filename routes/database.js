62bcaa5f9bdb7cea7b9d3854e4621e6d1fef0326
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 01:08:01 +0500

    Database fix Heroku

--- routes/database.js
+++ routes/database.js
@@ -1,35 +1,10 @@
 const knex = require("knex");
 
 const db = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "postgres",
-    password: "test",
-    database: "smart-brain",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
-
-// export const database = {
-//   users: [
-//     {
-//       id: "123",
-//       name: "John",
-//       email: "john@gmail.com",
-//       password: "john123",
-//       entries: 0,
-//       joined: new Date(),
-//     },
-//     {
-//       id: "456",
-//       name: "Smith",
-//       email: "smith@gmail.com",
-//       password: "smith456",
-//       entries: 0,
-//       joined: new Date(),
-//     },
-//   ],
-// };
 
 module.exports = db;
