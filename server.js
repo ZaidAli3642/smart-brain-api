622be9c8e75b84bdcea34b3d4283c858517a0f6e
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 02:21:56 +0500

    still fix

--- server.js
+++ server.js
@@ -8,16 +8,21 @@ const imageRoute = require("./routes/image");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+    credentials: true,
+  })
+);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.status(200).json("It's working!");
