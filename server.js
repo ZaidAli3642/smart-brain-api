3c60415c0a1a3342e45b76c7a1a4b95123d05ec3
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 02:29:00 +0500

    fix 2

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const imageRoute = require("./routes/image");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
