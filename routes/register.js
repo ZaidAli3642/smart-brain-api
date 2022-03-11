522d7de563892e982edf8ed68d05bd7e82e7a42f
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 20:03:44 +0500

    fix 24

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,7 @@ const db = require("./database");
 
 route.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  const saltRounds = 10;
+  const saltRounds = 5;
 
   if (!email || !name || !password) {
     return res.status(400).json("Incorrect Form Submission.");
@@ -16,38 +16,71 @@ route.post("/register", (req, res) => {
       return res.status(400).json(err);
     }
 
+    // Using trx as a query builder:
     db.transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
+      return trx
+        .insert({ hash: hash, email: email })
         .into("login")
         .returning("email")
         .then((loginEmail) => {
-          db("users")
+          return trx("users")
             .insert({
               email: loginEmail[0].email,
               name: name,
               joined: new Date(),
             })
-            .returning("*")
-            .then((user) => {
-              jwt.sign(user[0], "secret", (err, result) => {
-                if (err)
-                  return res.status(400).json("Error while generating token.");
-                console.log(err, result);
-                res.status(200).json("hello");
-              });
-            })
-            .catch((err) => res.status(400).json("Unable to register 1"));
+            .returning("*");
         })
         .then(trx.commit)
-        .catch((err) => {
-          res.status(400).json("Unable to register 3");
-          trx.rollback;
+        .catch(trx.rollback);
+    })
+      .then((inserts) => {
+        console.log(inserts);
+        jwt.sign(inserts[0], "secret", { expiresIn: "1h" }, (err, result) => {
+          res.status(200).json(result);
         });
-    }).catch((err) => res.status(400).json("Unable to register 2"));
+      })
+      .catch(function (error) {
+        // If we get here, that means that neither the 'Old Books' catalogues insert,
+        // nor any of the books inserts will have taken place.
+        res.status(400).json("Unable to Register");
+      });
+
+    // db.transaction((trx) => {
+    //   return trx
+    //     .insert({
+    //       hash: hash,
+    //       email: email,
+    //     })
+    //     .into("login")
+    //     .returning("email")
+    //     .then((loginEmail) => {
+    //       return db("users")
+    //         .insert({
+    //           email: loginEmail[0].email,
+    //           name: name,
+    //           joined: new Date(),
+    //         })
+    //         .returning("*")
+    //         .then((user) => {
+    //           jwt.sign(user[0], "secret", (err, result) => {
+    //             if (err)
+    //               return res.status(400).json("Error while generating token.");
+    //             console.log(err, result.length);
+    //             res.status(200).json("hello");
+    //             return;
+    //           });
+    //         })
+    //         .catch((err) => res.status(400).json("Unable to register 1"));
+    //     })
+    //     .then(trx.commit)
+    //     .catch((err) => {
+    //       res.status(400).json("Unable to register 3");
+    //       trx.rollback;
+    //     });
+    // })
+    //   .then((data) => console.log("hello " + data))
+    //   .catch((err) => res.status(400).json("Unable to register 2"));
   });
 });
 
