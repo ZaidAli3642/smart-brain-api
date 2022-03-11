8fea365d1a1eee9cda5b1781932fccb63c8f9721
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 14:58:59 +0500

    fix 8

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const signinRoute = require("./routes/signin");
 const registerRoute = require("./routes/register");
 const profileRoute = require("./routes/profile");
@@ -7,7 +6,6 @@ const imageRoute = require("./routes/image");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use(function (req, res, next) {
