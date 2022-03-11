f9be2e623e348ee7e8e6c89f06c962bdc4496f3f
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 19:28:37 +0500

    fix 20

--- routes/register.js
+++ routes/register.js
@@ -11,71 +11,71 @@ route.post("/register", (req, res) => {
     return res.status(400).json("Incorrect Form Submission.");
   }
 
-  // bcrypt.hash(password, saltRounds, (err, hash) => {
-  //   if (err) {
-  //     return res.status(400).json(err);
-  //   }
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
 
-  //   db.transaction((trx) => {
-  //     trx
-  //       .insert({
-  //         hash: hash,
-  //         email: email,
-  //       })
-  //       .into("login")
-  //       .returning("email")
-  //       .then((loginEmail) => {
-  //         db("users")
-  //           .insert({
-  //             email: loginEmail[0].email,
-  //             name: name,
-  //             joined: new Date(),
-  //           })
-  //           .returning("*")
-  //           .then((user) => {
-  //             jwt.sign(user[0], "secret", (err, result) => {
-  //               if (err) return new Error(err);
-  //               console.log(err, result);
-  //               res.status(200).json({ result: result });
-  //             });
-  //           })
-  //           .catch((err) => res.status(400).json(err));
-  //       })
-  //       .then(trx.commit)
-  //       .catch(trx.rollback);
-  //   }).catch((err) => res.status(400).json(err));
-
-  //   db.transaction((trx) => {
-  //     trx
-  //       .insert({
-  //         hash: hash,
-  //         email: email,
-  //       })
-  //       .into("login")
-  //       .returning("email")
-  //       .then((loginEmail) => {
-  //         db("users")
-  //           .insert({
-  //             email: loginEmail[0].email,
-  //             name: name,
-  //             joined: new Date(),
-  //           })
-  //           .returning("*")
-  //           .then((user) => {
-  //             jwt.sign(user[0], "secret", (err, result) => {
-  //               if (err) throw new Error(err);
-
-  //               return res.status(200).json(result);
-  //             });
-  //           })
-  //           .catch((err) => res.status(400).json(err));
-  //       })
-  //       .then(trx.commit)
-  //       .catch(trx.rollback);
-  //   }).catch((err) => res.status(400).json("Unable to Register!", err));
-  // });
+    db.transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          db("users")
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date(),
+            })
+            .returning("*")
+            .then((user) => {
+              jwt.sign(user[0], "secret", (err, result) => {
+                if (err) return new Error(err);
+                console.log(err, result);
+                res.status(200).json({ result: result });
+              });
+            })
+            .catch((err) => res.status(400).json(err));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    }).catch((err) => res.status(400).json(err));
+  });
 
   res.status(200).json("Hello");
 });
 
 module.exports = route;
+
+// db.transaction((trx) => {
+//     trx
+//       .insert({
+//         hash: hash,
+//         email: email,
+//       })
+//       .into("login")
+//       .returning("email")
+//       .then((loginEmail) => {
+//         db("users")
+//           .insert({
+//             email: loginEmail[0].email,
+//             name: name,
+//             joined: new Date(),
+//           })
+//           .returning("*")
+//           .then((user) => {
+//             jwt.sign(user[0], "secret", (err, result) => {
+//               if (err) throw new Error(err);
+
+//               return res.status(200).json(result);
+//             });
+//           })
+//           .catch((err) => res.status(400).json(err));
+//       })
+//       .then(trx.commit)
+//       .catch(trx.rollback);
+//   }).catch((err) => res.status(400).json("Unable to Register!", err));
