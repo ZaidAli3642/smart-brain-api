f819e2e75efc9f846c33e5a58a506735975c69a4
ZaidAli3642 <zaidali36422@gmail.com>
2022-03-11 22:19:31 +0500

    change

--- controllers/register.js
+++ controllers/register.js
@@ -3,36 +3,36 @@ const handleRegister = (req, res, db, bcrypt) => {
   if (!email || !name || !password) {
     return res.status(400).json("incorrect form submission");
   }
-  // const hash = bcrypt.hashSync(password);
-  //   db.transaction(trx => {
-  //     trx.insert({
-  //       hash: hash,
-  //       email: email
-  //     })
-  //     .into('login')
-  //     .returning('email')
-  //     .then(loginEmail => {
-  //       return trx('users')
-  //         .returning('*')
-  //         .insert({
-  //           // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-  //           // loginEmail[0] --> this used to return the email
-  //           // TO
-  //           // loginEmail[0].email --> this now returns the email
-  //           email: loginEmail[0].email,
-  //           name: name,
-  //           joined: new Date()
-  //         })
-  //         .then(user => {
-  //           res.json(user[0]);
-  //         })
-  //     })
-  //     .then(trx.commit)
-  //     .catch(trx.rollback)
-  //   })
-  //   .catch(err => res.status(400).json('unable to register'))
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+            // loginEmail[0] --> this used to return the email
+            // TO
+            // loginEmail[0].email --> this now returns the email
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 
-  return res.status(200).json("Hello");
+  // return res.status(200).json("Hello");
 };
 
 module.exports = {
